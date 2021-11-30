6dc36738fe28e2ba3518ca47a606a5313f7f703f
naramuratomofumi <tomofuminaramura@gmail.com>
2021-11-30 19:31:15 +0900

    ログインしていない場合、サインインページへ遷移することを再現

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe 'ユーザーログイン機能', type: :system do
   it 'ログインしていない状態でトップページにアクセスした場合、サインインページに移動する' do
     # トップページに遷移する
+    visit root_path
 
     # ログインしていない場合、サインインページに遷移していることを確認する
-
+    expect(current_path).to eq(new_user_session_path)
   end
+end
 
   it 'ログインに成功し、トップページに遷移する' do
     # 予め、ユーザーをDBに保存する
